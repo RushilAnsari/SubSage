674ab12d0136ac4fbf9f2215afe9f7fa0b295e17
Sanjam <sanjambedi.05@gmail.com>
2025-11-29 18:37:59 +0400

    Update Subscription.java

--- Subscription.java
+++ Subscription.java
@@ -1,3 +1,60 @@
-public class Subscription {
-    
+package subsage;
+
+import java.util.Date;
+
+public class Subscription implements Payable {
+
+    private String name;
+    private double price;
+    private String category;
+    private Date nextBillingDate;
+
+    public Subscription(String name, double price, String category, Date nextBillingDate) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.nextBillingDate = nextBillingDate;
+    }
+
+    public String getName() { 
+        return name; 
+    }
+
+    public void setName(String name) { 
+        this.name = name; 
+    }
+
+    public double getPrice() { 
+        return price; 
+    }
+
+    public void setPrice(double price) { 
+        this.price = price; 
+    }
+
+    public String getCategory() { 
+        return category; 
+    }
+
+    public void setCategory(String category) { 
+        this.category = category; 
+    }
+
+    public Date getNextBillingDate() { 
+        return nextBillingDate; 
+    }
+
+    public void setNextBillingDate(Date nextBillingDate) { 
+        this.nextBillingDate = nextBillingDate; 
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return name + " | " + price + " | " + nextBillingDate;
+    }
 }
